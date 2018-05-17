061d40a4793d6292fce7d5bb47e559f786f39414
Klement Streit <klement.streit@unibw.de>
2018-05-17 11:33:04 +0200

    first commit

---
+++ out/gcc-debug/src/routing/AODVQoSRouting.d
@@ -0,0 +1,271 @@
+out/gcc-debug//src/routing/AODVQoSRouting.o: \
+ src/routing/AODVQoSRouting.cc src/routing/AODVQoSRouting.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/common/INETDefs.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/common/precompiled_debug.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/common/precompiled.h \
+ /home/klement/Development/omnetpp-5.1.1/include/omnetpp.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/opp_defines.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/features.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/common/Compat.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/common/InitStages.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/networklayer/contract/IInterfaceTable.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/networklayer/common/L3Address.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/networklayer/contract/ipv4/IPv4Address.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/networklayer/contract/ipv6/IPv6Address.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/linklayer/common/MACAddress.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/networklayer/common/ModuleIdAddress.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/networklayer/common/ModulePathAddress.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/networklayer/common/InterfaceEntry.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/networklayer/common/InterfaceToken.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/common/NotifierConsts.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/networklayer/contract/IL3AddressType.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/networklayer/contract/INetworkProtocolControlInfo.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/networklayer/contract/IRoutingTable.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/networklayer/contract/IRoute.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/networklayer/contract/INetfilter.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/networklayer/contract/INetworkDatagram.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/common/lifecycle/ILifecycle.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/common/lifecycle/NodeStatus.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/transportlayer/contract/udp/UDPSocket.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/transportlayer/contract/udp/UDPControlInfo.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/transportlayer/contract/udp/UDPControlInfo_m.h \
+ src/routing/AODVQoSRouteData.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/transportlayer/udp/UDPPacket.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/transportlayer/contract/ITransportPacket.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/transportlayer/udp/UDPPacket_m.h \
+ src/routing/AODVQoSControlPackets_m.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/networklayer/ipv4/IPv4Datagram.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/networklayer/ipv4/IPv4Datagram_m.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/common/TLVOption.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/common/TLVOption_m.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/common/INETDefs.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/networklayer/common/IPProtocolId_m.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IRadio.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IPhysicalLayer.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/physicallayer/base/packetlevel/PhysicalLayerDefs.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/common/INETMath.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/common/Units.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IRadioFrame.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IArrival.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/common/geometry/common/Coord.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/common/geometry/common/EulerAngles.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IPrintableObject.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IRadioSignal.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/common/mapping/MappingBase.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/common/mapping/Interpolation.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/common/mapping/MappingUtils.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IListening.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IPhysicalLayerFrame.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IReception.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/bitlevel/ISignalAnalogModel.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/ITransmission.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IAntenna.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/mobility/contract/IMobility.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IReceiver.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IInterference.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/INoise.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IListeningDecision.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IReceptionDecision.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IReceptionResult.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/RadioControlInfo_m.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IModulation.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/ISNIR.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/ITransmitter.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/networklayer/ipv4/ICMPMessage.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/networklayer/ipv4/ICMPMessage_m.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/networklayer/ipv4/IPv4Route.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/linklayer/ideal/IdealMacFrame_m.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/linklayer/ieee80211/mac/Ieee80211Frame_m.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/linklayer/common/Ieee802Ctrl_m.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/common/BitVector.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/linklayer/csma/CSMAFrame_m.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/linklayer/base/MACFrameBase_m.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/linklayer/csmaca/CsmaCaMacFrame_m.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/linklayer/contract/IMACFrame.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/linklayer/lmac/LMacFrame_m.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/linklayer/bmac/BMacFrame_m.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/linklayer/xmac/XMacPkt_m.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/networklayer/common/IPSocket.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/common/ModuleAccess.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/common/lifecycle/NodeOperations.h \
+ /home/klement/Git/inetmanet-3.x/src/inet/common/lifecycle/LifecycleOperation.h
+
+src/routing/AODVQoSRouting.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/common/INETDefs.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/common/precompiled_debug.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/common/precompiled.h:
+
+/home/klement/Development/omnetpp-5.1.1/include/omnetpp.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/opp_defines.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/features.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/common/Compat.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/common/InitStages.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/networklayer/contract/IInterfaceTable.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/networklayer/common/L3Address.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/networklayer/contract/ipv4/IPv4Address.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/networklayer/contract/ipv6/IPv6Address.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/linklayer/common/MACAddress.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/networklayer/common/ModuleIdAddress.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/networklayer/common/ModulePathAddress.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/networklayer/common/InterfaceEntry.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/networklayer/common/InterfaceToken.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/common/NotifierConsts.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/networklayer/contract/IL3AddressType.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/networklayer/contract/INetworkProtocolControlInfo.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/networklayer/contract/IRoutingTable.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/networklayer/contract/IRoute.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/networklayer/contract/INetfilter.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/networklayer/contract/INetworkDatagram.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/common/lifecycle/ILifecycle.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/common/lifecycle/NodeStatus.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/transportlayer/contract/udp/UDPSocket.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/transportlayer/contract/udp/UDPControlInfo.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/transportlayer/contract/udp/UDPControlInfo_m.h:
+
+src/routing/AODVQoSRouteData.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/transportlayer/udp/UDPPacket.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/transportlayer/contract/ITransportPacket.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/transportlayer/udp/UDPPacket_m.h:
+
+src/routing/AODVQoSControlPackets_m.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/networklayer/ipv4/IPv4Datagram.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/networklayer/ipv4/IPv4Datagram_m.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/common/TLVOption.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/common/TLVOption_m.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/common/INETDefs.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/networklayer/common/IPProtocolId_m.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IRadio.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IPhysicalLayer.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/physicallayer/base/packetlevel/PhysicalLayerDefs.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/common/INETMath.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/common/Units.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IRadioFrame.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IArrival.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/common/geometry/common/Coord.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/common/geometry/common/EulerAngles.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IPrintableObject.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IRadioSignal.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/common/mapping/MappingBase.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/common/mapping/Interpolation.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/common/mapping/MappingUtils.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IListening.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IPhysicalLayerFrame.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IReception.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/bitlevel/ISignalAnalogModel.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/ITransmission.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IAntenna.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/mobility/contract/IMobility.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IReceiver.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IInterference.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/INoise.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IListeningDecision.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IReceptionDecision.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IReceptionResult.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/RadioControlInfo_m.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/IModulation.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/ISNIR.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/physicallayer/contract/packetlevel/ITransmitter.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/networklayer/ipv4/ICMPMessage.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/networklayer/ipv4/ICMPMessage_m.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/networklayer/ipv4/IPv4Route.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/linklayer/ideal/IdealMacFrame_m.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/linklayer/ieee80211/mac/Ieee80211Frame_m.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/linklayer/common/Ieee802Ctrl_m.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/common/BitVector.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/linklayer/csma/CSMAFrame_m.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/linklayer/base/MACFrameBase_m.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/linklayer/csmaca/CsmaCaMacFrame_m.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/linklayer/contract/IMACFrame.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/linklayer/lmac/LMacFrame_m.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/linklayer/bmac/BMacFrame_m.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/linklayer/xmac/XMacPkt_m.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/networklayer/common/IPSocket.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/common/ModuleAccess.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/common/lifecycle/NodeOperations.h:
+
+/home/klement/Git/inetmanet-3.x/src/inet/common/lifecycle/LifecycleOperation.h:
---
+++ out/gcc-debug/src/routing/QoSRequirementsMapping.d
@@ -0,0 +1,5 @@
+out/gcc-debug//src/routing/QoSRequirementsMapping.o: \
+ src/routing/QoSRequirementsMapping.cc \
+ src/routing/QoSRequirementsMapping.h
+
+src/routing/QoSRequirementsMapping.h:
